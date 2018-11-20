47ccb8df7674ab36613f9b4805eabca3a64421ab
heliance <malleus.sigmaris@gmail.com>
2018-11-20 16:16:41 +0300

    Added LCD scripts.

---
+++ lcd_write.py
@@ -0,0 +1,27 @@
+import subprocess
+from time import sleep
+from Adafruit_CharLCD import Adafruit_CharLCD
+
+
+call = './reset_ica.sh'
+subprocess.call(call)
+
+lcd = Adafruit_CharLCD(rs=26, en=19, d4=13, d5=27, d6=17, d7=22,
+                       cols=16, lines=2)
+
+while True:
+    call2 = """ curl -G -k -s "https://172.16.0.225:8086/query?db=lora_gw" \
+    --data-urlencode "q=SELECT last(value) FROM "humidity" 
+    WHERE ("GW_ID" = '1')" """
+    db_query = str(subprocess.check_output(call2, shell=True))
+    humidity = db_query[131:136]
+
+    call3 = """ curl -G -k -s "https://172.16.0.225:8086/query?db=lora_gw" \
+    --data-urlencode "q=SELECT last(value) FROM "temperature" 
+    WHERE ("GW_ID" = '1')" """
+    db_query = str(subprocess.check_output(call3, shell=True))
+    temp = db_query[134:139]
+
+    lcd.clear()
+    lcd.message('Temp.: ' + temp + 'C' + '\nHumidity: ' + humidity + '%')
+    sleep(60)
---
+++ lcd_write_si7021.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import smbus
+import time
+import subprocess
+from Adafruit_CharLCD import Adafruit_CharLCD
+
+bus = smbus.SMBus(1)
+lcd = Adafruit_CharLCD(rs=26, en=19, d4=13, d5=27, d6=17, d7=22,
+                       cols=16, lines=2)
+call = './reset_ica.sh'
+subprocess.call(call)
+
+while True:
+    bus.write_byte(0x40, 0xF5)
+    time.sleep(0.3)
+
+    data0 = bus.read_byte(0x40)
+    data1 = bus.read_byte(0x40)
+
+    humidity = str(((data0 * 256 + data1) * 125 / 65536.0) - 6)
+    time.sleep(0.3)
+
+    bus.write_byte(0x40, 0xF3)
+    time.sleep(0.3)
+
+    data0 = bus.read_byte(0x40)
+    data1 = bus.read_byte(0x40)
+
+    cTemp = str(((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85)
+    # fTemp = cTemp * 1.8 + 32
+
+    lcd.clear()
+    lcd.message('Temp.: ' + cTemp[:5] + 'C' + '\nHumidity: '
+                + humidity[:5] + '%')
+    time.sleep(60)
